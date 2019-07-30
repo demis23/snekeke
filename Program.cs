2d533648a975e855bafc79f89211fc5f1adff90e
demis23 <office.falkon.tour@gmail.com>
2019-07-30 21:45:28 +0300

    New class HorLine

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace snakeke
 {
@@ -8,12 +12,14 @@ namespace snakeke
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-           
             p1.Draw();
 
             Point p2 = new Point(4,5,'#');
-           
             p2.Draw();
+
+            List<Point> pointList = new List<Point>();
+            pointList.Add(p1);
+            pointList.Add(p2);
             Console.ReadLine();
         }
     }
