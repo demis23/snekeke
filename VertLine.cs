8095aed04c874514384bf2183251ac86cb89608c
demis23 <office.falkon.tour@gmail.com>
2019-07-30 21:54:44 +0300

    new class VertLine

---
+++ VertLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snakeke
+{
+    class VertLine
+    {
+        List<Point> pList;
+
+        public VertLine(int yTop, int yDown, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yTop; y <= yDown; y++)
+            {
+                Point p = new Point(y, y, sym);
+                pList.Add(p);
+
+            }
+        }
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
